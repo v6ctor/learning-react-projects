e0a8d6eda067bdb9f36b5a0a9f12b2053be35376
v6ctor <victorsumano27@gmail.com>
2023-12-29 22:58:28 -0800

    Add prop type validation and improve reusability

--- airbnb-experiences/src/components/Card.jsx
+++ airbnb-experiences/src/components/Card.jsx
@@ -1,24 +1,40 @@
 import star from '/star.png'
-import person from '/person.png'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Card() {
-  return (
-    <section className='card'>
-      <img className='card__img-person' src={person}></img>
-      <div className='card__information'>
-        <div className='card__information--ratings'>
-          <img className='card__information--ratings__star' src={star}></img>
-          <p className='card__information--ratings__text'>5.0
-            <span> (6) &#183; USA</span>
-          </p>
-        </div>
-        <div className='card__information--activity'>
-          <p className='card__information--activity__text'>Life lessons with Katie Zaferes</p>
-          <div className='card__information--activity__pricing'>
-            <span>From $136</span> / person
+class Card extends React.Component {
+  render() {
+    const { img } = this.props
+    console.log('/' + img)
+    return (
+      <section className='card'>
+        <img className='card__img-person' src={'/' + img}></img>
+        <div className='card__information'>
+          <div className='card__information--ratings'>
+            <img className='card__information--ratings__star' src={star}></img>
+            <p className='card__information--ratings__text'>{this.props.rating}
+              <span> {this.props.reviewCount} &#183; {this.props.country}</span>
+            </p>
+          </div>
+          <div className='card__information--activity'>
+            <p className='card__information--activity__text'>{this.props.title}</p>
+            <div className='card__information--activity__pricing'>
+              <span>From ${this.props.price}</span> / person
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 }
+
+Card.propTypes = {
+  img: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired,
+  reviewCount: PropTypes.number.isRequired,
+  country: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+}
+
+export default Card
